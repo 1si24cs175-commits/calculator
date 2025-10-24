c00a1dc21839dad514d4aa828ca1ad6a3a38371a
Bhanu20061SI24CS032 <bhanusrgowda@gmail.com>
2025-10-24 08:38:14 +0530

    Created the calculator program

---
+++ script.js
@@ -0,0 +1,30 @@
+
+
+let display = document.getElementById('display');
+        let currentInput = '';
+
+        function appendValue(val) {
+            if (display.innerText === '0' && val !== '.') {
+                currentInput = '';
+            }
+            currentInput += val;
+            display.innerText = currentInput;
+        }
+
+        function clearDisplay() {
+            currentInput = '';
+            display.innerText = '0';
+        }
+
+        function calculateResult() {
+            try {
+                // Evaluate, but replace ×, ÷, − with JS operators
+                let expression = currentInput.replace(/×/g, '*').replace(/÷/g, '/').replace(/−/g, '-');
+                let result = eval(expression);
+                display.innerText = result;
+                currentInput = result.toString();
+            } catch (e) {
+                display.innerText = 'Error';
+                currentInput = '';
+            }
+        }
